a226605e8ec9749f1847a62735ae2135808e3331
carl <karol-nowakowski@o2.pl>
2021-04-23 12:18:21 +0200

    Use GlobalStyle from styled-components

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+*, *::before , *::after {
+    box-sizing: border-box;
+}
+
+html {
+    font-size: 62,5%; //1 rem === 10px
+}
+body {
+    background-color:yellow;
+    font-size: 1.6rem; //happy rems
+    font-family: 'Montserrat', sans-serif;
+}
+`;
+
+export default GlobalStyle;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 function Root() {
   return (
     <div>
+      <GlobalStyle />
       <h1>Hello Karol</h1>
-      <Button>Close / Save</Button>
+      <Button width='500px'>Close / Save</Button>
       <Button secondary>Remove</Button>
     </div>
   );
