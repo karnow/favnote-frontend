053568df016b6b86a8c7e622c9e6c0b5533cb87d
carl <karol-nowakowski@o2.pl>
2021-04-23 11:06:03 +0200

    Refactoring and create Button component

---
+++ src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+function Root() {
+  return (
+    <div>
+      <h1>Hello Karol</h1>
+      <Button>Close / Save</Button>
+      <Button secondary>Remove</Button>
+    </div>
+  );
+}
+
+export default Root;
